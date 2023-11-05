8c5f3fc6918c5a8be75f7073d4b4a834956dee79
pierrickmartino <pierrick.martino@gmail.com>
2023-11-05 14:53:28 +0100

    review db model

--- blockbuilders/models.py
+++ blockbuilders/models.py
@@ -5,12 +5,18 @@ from django.contrib.auth.models import User
 class Blockchain(Enum):
     POLYGON = "Polygon"
 
+class TypeTransaction(Enum):
+    BUY = "Buy"
+    SEL = "Sell"
+    DEP = "Deposit"
+    WTH = "Withdrawal"
+
 class Wallet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="user_wallets")
-    address = models.CharField(max_length=255) 
-    name = models.CharField(max_length=255, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_wallets")
+    address = models.CharField(max_length=255, default="") 
+    name = models.CharField(max_length=255, default="")
     balance = models.DecimalField(max_digits=15, decimal_places=2, default=0) # type: ignore
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, default="")
     sys_creation_date = models.DateTimeField(auto_now_add=True)
     sys_update_date = models.DateTimeField(auto_now=True)
 
@@ -24,12 +30,12 @@ class Wallet(models.Model):
     
 class Blockchain(models.Model):
     name = models.CharField(max_length=255, choices=[(tag.name, tag.value) for tag in Blockchain])
-    icon = models.CharField(max_length=255)
+    icon = models.CharField(max_length=255, default="")
     is_active = models.BooleanField()
-    main_url = models.CharField(max_length=255)
-    explorer_url = models.CharField(max_length=255)
-    contract_url = models.CharField(max_length=255)
-    transaction_url = models.CharField(max_length=255)
+    main_url = models.CharField(max_length=255, default="")
+    explorer_url = models.CharField(max_length=255, default="")
+    contract_url = models.CharField(max_length=255, default="")
+    transaction_url = models.CharField(max_length=255, default="")
     sys_creation_date = models.DateTimeField(auto_now_add=True)
     sys_update_date = models.DateTimeField(auto_now=True)
 
@@ -37,11 +43,11 @@ class Blockchain(models.Model):
         return f"{self.name}"
     
 class Contract(models.Model):
-    address = models.CharField(max_length=255) 
+    address = models.CharField(max_length=255, default="") 
     blockchain = models.ForeignKey(Blockchain, on_delete=models.DO_NOTHING, related_name="blockchain_contracts")
-    name = models.CharField(max_length=255, blank=True, null=True)
-    symbol = models.CharField(max_length=10, blank=True, null=True)
-    price = models.DecimalField(max_digits=15, decimal_places=8, default=0) # type: ignore
+    name = models.CharField(max_length=255, default="")
+    symbol = models.CharField(max_length=10, default="")
+    price = models.DecimalField(max_digits=15, decimal_places=8, default=0)
     # market_cap = models.DecimalField(max_digits=20, decimal_places=2, default=0) # type: ignore
     # volume = models.DecimalField(max_digits=20, decimal_places=10, default=0) # type: ignore
     sys_creation_date = models.DateTimeField(auto_now_add=True)
@@ -56,8 +62,8 @@ class Contract(models.Model):
         return f"{self.name}"
     
 class ContractLink(models.Model):
-    contract = models.ForeignKey(Contract, on_delete=models.DO_NOTHING, related_name="contract_contractlinks")
-    wallet = models.ForeignKey(Wallet, on_delete=models.DO_NOTHING, related_name="wallet_contractlinks")
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name="contract_contractlinks")
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="wallet_contractlinks")
     is_active = models.BooleanField()
     sys_creation_date = models.DateTimeField(auto_now_add=True)
     sys_update_date = models.DateTimeField(auto_now=True)
@@ -68,3 +74,53 @@ class ContractLink(models.Model):
 		
     def __str__(self):
         return f"{self.wallet} - {self.contract}"
+
+class ContractCalculator():
+    def __init__(self, contract):
+        self.contract = contract
+
+class Position(models.Model):
+    contract_link = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name="contractlink_positions")
+    quantity = models.DecimalField(max_digits=32, decimal_places=18)
+    amount = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    sys_creation_date = models.DateTimeField(auto_now_add=True)
+    sys_update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Position"
+        verbose_name_plural = "Positions"
+		
+    def __str__(self):
+        return f"{self.contract_link} - {self.quantity}"
+
+class PositionCalculator():
+    def __init__(self, position):
+        self.position = position
+    def total(self):
+        return sum(i.amount for i in self.position.items.all())
+
+class Transaction(models.Model):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="position_transactions")
+    type = models.CharField(max_length=3, choices=[(tag.name, tag.value) for tag in TypeTransaction])
+    quantity = models.DecimalField(max_digits=18, decimal_places=8)
+    price = models.DecimalField(max_digits=15, decimal_places=8)
+    cost = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    date = models.DateTimeField(db_index=True)
+    comment = models.TextField(default="")
+    hash = models.CharField(max_length=255, default="")
+    sys_creation_date = models.DateTimeField(auto_now_add=True)
+    sys_update_date = models.DateTimeField(auto_now=True)
+
+    # Meta and String
+    class Meta:
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"
+        ordering = ("date",)
+
+    def __str__(self):
+        return f"{self.hash} - {self.type} {self.quantity} ({self.cost}) - {self.date}"
+    
+class TransactionCalculator():
+    def __init__(self, transaction):
+        self.transaction = transaction
+    
