e3a0a288a0b76a1aa2d12c6c35a5a73413d7bfb5
pierrickmartino <pierrick.martino@gmail.com>
2024-04-16 17:35:07 +0200

    fix bug when add a wallet

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from .models import Contract, Wallet
 class WalletForm(forms.ModelForm):
     class Meta:
         model = Wallet
-        fields = ['address']
+        fields = ['address','name']
 
 class ContractForm(forms.ModelForm):
     class Meta:
