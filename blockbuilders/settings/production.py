aca1e35b8797fb0409c5a669da6bab96b55ac7ea
pierrickmartino <pierrick.martino@gmail.com>
2024-06-28 06:59:36 +0200

    update for live server deployment

--- blockbuilders/settings/production.py
+++ blockbuilders/settings/production.py
@@ -15,3 +15,9 @@ CACHES = {
         "LOCATION": config("REDIS_BACKEND"),
     },
 }
+
+# Media and Static files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles")
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")
