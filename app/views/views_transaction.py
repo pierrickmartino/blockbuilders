770a1786dd076c6f863e8589add7b44a147a1825
pierrickmartino <pierrick.martino@gmail.com>
2024-08-13 16:40:11 +0200

    add chart to transaction page

--- app/views/views_transaction.py
+++ app/views/views_transaction.py
@@ -4,15 +4,24 @@ import logging, os
 
 from django.http import HttpResponse
 
+import pandas as pd
+import pandas_ta as ta
+import mplfinance as mpf
+from io import BytesIO
+import base64
+
 logger = logging.getLogger("blockbuilders")
 
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from datetime import datetime, timedelta
 
 from app.models import (
     Contract,
     ContractCalculator,
+    MarketData,
     Position,
     PositionCalculator,
     Transaction,
@@ -103,6 +112,68 @@ def export_transactions_csv(request, position_id):
     return response
 
 
+def plot_candlestick(data):
+    if not data:
+        return None
+    df = pd.DataFrame(data)
+    df.set_index("timestamp", inplace=True)
+
+    # Calculate Moving Averages
+    df["EMA20"] = ta.ema(df["close"], length=20)
+    df["EMA50"] = ta.ema(df["close"], length=50)
+
+    # Calculate RSI
+    df["RSI"] = ta.rsi(df["close"], length=14)
+
+    # Calculate MACD
+    # macd = ta.macd(df['close'], fast=12, slow=26, signal=9)
+    # df['MACD'] = macd['MACD_12_26_9']
+    # df['MACD_Signal'] = macd['MACDs_12_26_9']
+    # df['MACD_Hist'] = macd['MACDh_12_26_9']
+
+    # Plotting
+    mc = mpf.make_marketcolors(up="#00bcd4", down="#ff9800", inherit=True)
+    s = mpf.make_mpf_style(
+        marketcolors=mc,
+        rc={"axes.labelsize": "small"},
+        base_mpf_style="ibd",
+        facecolor="#ffffff",
+        gridstyle="dotted",
+        gridcolor="#323233",
+        gridaxis="both",
+    )
+
+    fig, axes = mpf.plot(
+        df,
+        type="candle",
+        style=s,
+        # title='Price with Technical Indicators',
+        ylabel="",
+        volume=False,
+        # mav=(20, 50),
+        addplot=[
+            mpf.make_addplot(df["EMA20"], color="blue"),
+            mpf.make_addplot(df["EMA50"], color="red"),
+            # mpf.make_addplot(df['RSI'], panel=2, color='purple', ylabel='RSI'),
+        ],
+        returnfig=True,
+        # figsize=(14, 10),  # Increase graph size
+        figscale=1.0,
+        figratio=(18, 10),
+        yscale="log",
+        datetime_format=' %d.%m.%Y',
+        # tight_layout=True
+    )
+
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    buf.seek(0)
+    image_png = buf.getvalue()
+    buf.close()
+    image_b64 = base64.b64encode(image_png).decode("utf-8")
+    return image_b64
+
+
 @login_required
 def position_transactions_paginated(request, position_id, page):
     position = Position.objects.filter(id=position_id).first()
@@ -111,6 +182,28 @@ def position_transactions_paginated(request, position_id, page):
     transactions = Transaction.objects.filter(position=position).order_by("-date")
     position_calculator = PositionCalculator(position)
     position_amount = position_calculator.calculate_amount()
+
+    symbol_for_plot = contract.symbol.replace("WETH", "ETH")
+
+    delta = 180
+    days_ago = timezone.now().date() - timedelta(days=delta)
+    market_data = MarketData.objects.filter(symbol=symbol_for_plot, reference="USD", time__gte=days_ago).order_by("time")
+    stock_data = []
+    for data in market_data:
+        stock_data.append(
+            {
+                "timestamp": data.time,
+                "open": float(data.open),
+                "high": float(data.high),
+                "low": float(data.low),
+                "close": float(data.close),
+                "volume": int(data.volume_to),
+            }
+        )
+
+    chart = plot_candlestick(stock_data) if stock_data else None
+    # if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    #     return JsonResponse({'chart': chart, 'error': 'Invalid stock symbol or data not available.' if not chart else ''})
 
     contract_calculator = ContractCalculator(position.contract)
     daily_price_delta = contract_calculator.calculate_daily_price_delta()
@@ -159,9 +252,7 @@ def position_transactions_paginated(request, position_id, page):
     page_transactions = paginator.get_page(page)
     page_transactions.adjusted_elided_pages = paginator.get_elided_page_range(page)
 
-    reference_avg_cost = (
-        TransactionCalculator(transactions.first()).calculate_avg_cost() if transactions else 0
-    )
+    reference_avg_cost = TransactionCalculator(transactions.first()).calculate_avg_cost() if transactions else 0
     total_unrealized_gain = (
         (
             (contract.price - reference_avg_cost) / reference_avg_cost * 100
@@ -172,8 +263,6 @@ def position_transactions_paginated(request, position_id, page):
         else 0
     )
 
-    # logger.info(f"Performance information :  {total_unrealized_gain} /  {total_realized_gain}")
-
     context = {
         "page_transactions": page_transactions,
         "position": position,
@@ -183,6 +272,7 @@ def position_transactions_paginated(request, position_id, page):
         "total_realized_gain": total_realized_gain,
         "position_amount": position_amount,
         "position_kpi": position_kpi,
+        "chart": chart,
     }
     return render(request, "transactions.html", context)
 
