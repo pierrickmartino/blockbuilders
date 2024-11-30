fbede12e8c9d01e22e3a38f2f20035fea4f84542
pierrickmartino <pierrick.martino@gmail.com>
2024-11-30 12:36:18 +0100

    Datagrid operationnelle

--- frontend/app/dashboard/components/dashboard/WalletTable.tsx
+++ frontend/app/dashboard/components/dashboard/WalletTable.tsx
@@ -1,41 +1,26 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 
 import {
   Typography,
   Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
   Chip,
-  TableContainer,
-  IconButton,
-  Menu,
-  MenuItem,
-  ListItemIcon,
-  TablePagination,
   Button,
   Card,
   CardContent,
   Stack,
-  CardActions,
+  CardHeader,
 } from "@mui/material";
-import DashboardCard from "../shared/DashboardCard";
 import formatNumber from "@/app/utils/formatNumber";
-import { IconDotsVertical } from "@tabler/icons-react";
 import { Wallet } from "@/app/lib/definition";
 import { Add, Edit, EventRepeat, Refresh } from "@mui/icons-material";
 import { Download } from "@mui/icons-material";
 import { Delete } from "@mui/icons-material";
-import { Visibility } from "@mui/icons-material";
 import {
   deleteWallet,
   downloadWallet,
   refreshWallet,
   refreshFullWallet,
 } from "@/app/lib/actions";
-import PerformanceChip from "../../ui-components/chips/PerformanceChip";
 import { DataGrid, GridActionsCellItem, GridColDef } from "@mui/x-data-grid";
 
 // Define the props type that will be passed into WalletTable
@@ -66,55 +51,6 @@ const WalletTable: React.FC<WalletTableProps> = ({
   onWalletFullRefreshed,
   onCreateWallet,
 }) => {
-  const walletMenuItems = [
-    {
-      title: "See details",
-      key: "wallet-details",
-      value: "wallet-details",
-      button: <Visibility fontSize="small" />,
-    },
-    {
-      title: "Edit wallet",
-      key: "wallet-edit",
-      value: "wallet-edit",
-      button: <Edit fontSize="small" />,
-    },
-    {
-      title: "Download history",
-      key: "wallet-download",
-      value: "wallet-download",
-      button: <Download fontSize="small" />,
-    },
-    {
-      title: "Refresh price",
-      key: "wallet-refresh",
-      value: "wallet-refresh",
-      button: <Refresh fontSize="small" />,
-    },
-    {
-      title: "Full refresh",
-      key: "wallet-refresh-full",
-      value: "wallet-refresh-full",
-      button: <EventRepeat fontSize="small" />,
-    },
-    {
-      title: "Delete wallet",
-      key: "wallet-delete",
-      value: "wallet-delete",
-      button: <Delete fontSize="small" />,
-    },
-  ];
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [selectedWalletId, setSelectedWalletId] = useState<string | null>(null); // Add state to track wallet ID
-  const open = Boolean(anchorEl);
-
-  const handleClick = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    wallet_id: string
-  ) => {
-    setAnchorEl(event.currentTarget);
-    setSelectedWalletId(wallet_id);
-  };
 
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement> | null,
@@ -129,20 +65,15 @@ const WalletTable: React.FC<WalletTableProps> = ({
     onRowsPerPageChange(parseInt(event.target.value, 10)); // Call the passed prop to update the rows per page state
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-    setSelectedWalletId(null);
-  };
-
   // Handle navigation to wallet details
-  const handleNavigateToDetails = () => {
+  const handleNavigateToDetails = (selectedWalletId : string) => {
     if (selectedWalletId !== null) {
       window.location.href = `/dashboard/wallets/${selectedWalletId}/positions`;
     }
   };
 
   // Handle navigation to wallet details
-  const handleDeletion = async () => {
+  const handleDeletion = async (selectedWalletId : string) => {
     if (selectedWalletId !== null) {
       const response = await deleteWallet(selectedWalletId.toString());
       if (response.message !== "Database Error: Failed to delete wallet.") {
@@ -151,7 +82,7 @@ const WalletTable: React.FC<WalletTableProps> = ({
     }
   };
 
-  const handleDownload = async () => {
+  const handleDownload = async (selectedWalletId : string) => {
     if (selectedWalletId !== null) {
       const response = await downloadWallet(selectedWalletId.toString());
       if (response.task_id) {
@@ -163,7 +94,7 @@ const WalletTable: React.FC<WalletTableProps> = ({
     }
   };
 
-  const handleRefresh = async () => {
+  const handleRefresh = async (selectedWalletId : string) => {
     if (selectedWalletId !== null) {
       const response = await refreshWallet(selectedWalletId.toString());
       if (response.task_id) {
@@ -174,7 +105,7 @@ const WalletTable: React.FC<WalletTableProps> = ({
     }
   };
 
-  const handleRefreshFull = async () => {
+  const handleRefreshFull = async (selectedWalletId : string) => {
     if (selectedWalletId !== null) {
       const response = await refreshFullWallet(selectedWalletId.toString());
       if (response.task_id) {
@@ -246,12 +177,13 @@ const WalletTable: React.FC<WalletTableProps> = ({
     {
       field: "actions",
       type: "actions",
-      width: 80,
+      width: 100,
       getActions: (cell) => [
         <GridActionsCellItem
           key="wallet-details"
           label="See details"
-          onClick={() => handleNavigateToDetails()}
+          icon={<Edit fontSize="small"  />}
+          onClick={() => handleNavigateToDetails(cell.id.toString())}
           showInMenu
         />,
         // <GridActionsCellItem
@@ -262,25 +194,29 @@ const WalletTable: React.FC<WalletTableProps> = ({
         <GridActionsCellItem
           key="wallet-download"
           label="Download history"
-          onClick={() => handleDownload()}
+          icon={<Download fontSize="small" />}
+          onClick={() => handleDownload(cell.id.toString())}
           showInMenu
         />,
         <GridActionsCellItem
           key="wallet-refresh"
           label="Refresh price"
-          onClick={() => handleRefresh()}
+          icon={<Refresh fontSize="small" />}
+          onClick={() => handleRefresh(cell.id.toString())}
           showInMenu
         />,
         <GridActionsCellItem
           key="wallet-refresh-full"
           label="Full refresh"
-          onClick={() => handleRefreshFull()}
+          icon={<EventRepeat fontSize="small" />}
+          onClick={() => handleRefreshFull(cell.id.toString())}
           showInMenu
         />,
         <GridActionsCellItem
           key="wallet-delete"
           label="Delete wallet"
-          onClick={() => handleDeletion()}
+          icon={<Delete fontSize="small" />}
+          onClick={() => handleDeletion(cell.id.toString())}
           showInMenu
         />,
       ],
@@ -305,7 +241,7 @@ const WalletTable: React.FC<WalletTableProps> = ({
 
   return (
     <Card variant="outlined" sx={{ height: "100%", flexGrow: 1 }}>
-      <CardActions>{action}</CardActions>
+      <CardHeader action={action}></CardHeader>
       <CardContent>
         <Typography component="h2" variant="subtitle2" gutterBottom>
           Wallet Overview
@@ -320,163 +256,6 @@ const WalletTable: React.FC<WalletTableProps> = ({
             </Typography>
           </Stack>
           <Box>
-            <TableContainer
-              sx={{
-                width: {
-                  xs: "274px",
-                  sm: "100%",
-                },
-              }}
-            >
-              <Table
-                size="small"
-                sx={{
-                  whiteSpace: "nowrap",
-                  mt: 0,
-                }}
-              >
-                <TableHead>
-                  <TableRow>
-                    {/* <TableCell>
-                  <Typography color="textSecondary" variant="h6">
-                    Id
-                  </Typography>
-                </TableCell> */}
-                    <TableCell>
-                      <Typography variant="h6">Name</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="h6">Description</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="h6">Address</Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography variant="h6">Balance</Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography variant="h6">Capital Gain</Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography variant="h6">UnRealized</Typography>
-                    </TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {wallets.map((wallet: Wallet) => (
-                    <TableRow key={wallet.id}>
-                      {/* <TableCell>
-                    <Typography fontSize="14px" fontWeight={500}>
-                      {wallet.id}
-                    </Typography>
-                  </TableCell> */}
-                      <TableCell>
-                        <Box display="flex" alignItems="center">
-                          <Box>
-                            <Typography fontWeight={500}>
-                              {wallet.name}
-                            </Typography>
-                          </Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell>
-                        <Box display="flex" alignItems="center">
-                          <Box>
-                            <Typography color="textSecondary">
-                              {wallet.description}
-                            </Typography>
-                          </Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell>
-                        <Box display="flex" alignItems="center">
-                          <Box>
-                            <Typography>
-                              {truncateText(wallet.address, 15)}
-                            </Typography>
-                          </Box>
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        <Typography>
-                          {formatNumber(wallet.balance, "currency")}
-                        </Typography>
-                      </TableCell>
-                      <TableCell align="right">
-                        <PerformanceChip
-                          input={wallet.capital_gain}
-                          type="currency"
-                        />
-                      </TableCell>
-                      <TableCell align="right">
-                        <PerformanceChip
-                          input={wallet.unrealized_gain}
-                          type="percentage"
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <IconButton
-                          id="basic-button"
-                          aria-controls={open ? "basic-menu" : undefined}
-                          aria-haspopup="true"
-                          aria-expanded={open ? "true" : undefined}
-                          onClick={(event) => handleClick(event, wallet.id)}
-                          aria-label="Open to show more"
-                          title="Open to show more"
-                        >
-                          <IconDotsVertical width={18} />
-                        </IconButton>
-                        <Menu
-                          id="basic-menu"
-                          anchorEl={anchorEl}
-                          keepMounted
-                          open={open}
-                          onClose={handleClose}
-                        >
-                          {walletMenuItems.map((item) => (
-                            <MenuItem
-                              onClick={() => {
-                                handleClose();
-                                if (item.key === "wallet-details") {
-                                  handleNavigateToDetails();
-                                }
-                                if (item.key === "wallet-delete") {
-                                  handleDeletion();
-                                }
-                                if (item.key === "wallet-download") {
-                                  handleDownload();
-                                }
-                                if (item.key === "wallet-refresh") {
-                                  handleRefresh();
-                                }
-                                if (item.key === "wallet-refresh-full") {
-                                  handleRefreshFull();
-                                }
-                              }}
-                              key={item.key}
-                              value={item.value}
-                            >
-                              <ListItemIcon>{item.button}</ListItemIcon>
-                              {item.title}
-                            </MenuItem>
-                          ))}
-                        </Menu>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              component="div"
-              rowsPerPageOptions={[5, 10]}
-              count={totalCount}
-              page={page}
-              rowsPerPage={rowsPerPage}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
             <DataGrid
               autoHeight
               checkboxSelection
